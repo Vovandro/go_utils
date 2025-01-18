0b2633d87aec3bc2456feb9545684bc46c1a99cf
Vladimir <vovandro@gmail.com>
2025-01-18 19:42:44 +0300

    Refactor parsing logic to improve type handling.
    Enhanced the `Parse` method invocation to better handle target types by checking both pointer and non-pointer receivers. This ensures more robust and flexible parsing for string-to-type conversions while addressing potential method resolution issues.

--- decode/decode.go
+++ decode/decode.go
@@ -181,10 +181,15 @@ func convertBasicTypes(source reflect.Value, targetType reflect.Type) (reflect.V
 		} else {
 			switch source.Kind() {
 			case reflect.String:
-				if m, ok := targetType.MethodByName("Parse"); ok && m.Type.NumIn() == 1 && m.Type.In(0).Kind() == reflect.String {
-					newValue := reflect.New(targetType).Elem()
-					newValue.MethodByName("Parse").Call([]reflect.Value{reflect.ValueOf(source.String())})
-					return newValue, nil
+				newValue := reflect.New(targetType)
+
+				if m := newValue.Elem().MethodByName("Parse"); !m.IsNil() && m.Type().NumIn() == 1 && m.Type().In(0).Kind() == reflect.String &&
+					m.Type().NumOut() == 1 && m.Type().Out(0) == targetType {
+					res := newValue.Elem().MethodByName("Parse").Call([]reflect.Value{reflect.ValueOf(source.String())})
+					return res[0], nil
+				} else if m := newValue.MethodByName("Parse"); !m.IsNil() && m.Type().NumIn() == 1 && m.Type().In(0).Kind() == reflect.String {
+					m.Call([]reflect.Value{reflect.ValueOf(source.String())})
+					return newValue.Elem(), nil
 				} else {
 					if intValue, err := strconv.ParseInt(source.String(), 10, 64); err == nil {
 						return reflect.ValueOf(intValue).Convert(targetType), nil
@@ -205,15 +210,20 @@ func convertBasicTypes(source reflect.Value, targetType reflect.Type) (reflect.V
 	case reflect.Float32, reflect.Float64:
 		switch source.Kind() {
 		case reflect.String:
-			if m, ok := targetType.MethodByName("Parse"); ok && m.Type.NumIn() == 1 && m.Type.In(0).Kind() == reflect.String {
-				newValue := reflect.New(targetType).Elem()
-				newValue.MethodByName("Parse").Call([]reflect.Value{reflect.ValueOf(source.String())})
-				return newValue, nil
+			newValue := reflect.New(targetType)
+
+			if m := newValue.Elem().MethodByName("Parse"); !m.IsNil() && m.Type().NumIn() == 1 && m.Type().In(0).Kind() == reflect.String &&
+				m.Type().NumOut() == 1 && m.Type().Out(0) == targetType {
+				res := newValue.Elem().MethodByName("Parse").Call([]reflect.Value{reflect.ValueOf(source.String())})
+				return res[0], nil
+			} else if m := newValue.MethodByName("Parse"); !m.IsNil() && m.Type().NumIn() == 1 && m.Type().In(0).Kind() == reflect.String {
+				m.Call([]reflect.Value{reflect.ValueOf(source.String())})
+				return newValue.Elem(), nil
 			} else {
 				if intValue, err := strconv.ParseFloat(source.String(), 64); err == nil {
 					return reflect.ValueOf(intValue).Convert(targetType), nil
 				} else {
-					return reflect.Value{}, err
+					return newValue.Elem(), err
 				}
 			}
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
